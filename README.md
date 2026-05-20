e7cdf38007a3d3c02b61f0c13a1fff190a6abbcd
alko <alko@alk0.ru>
2026-05-20 16:47:58 +0500

    local build

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 Keyboard Maintainer: [alko](https://github.com/alko-kbd/zmk-cornedeon54) [alko-kbd@alk0.ru](mailto:alko-kbd@alk0.ru)
 
 Web Site: [cornedeon.ru](https://cornedeon.ru)
+
+**Local build (devcontainer)**
+
+#workspaces/zmk# ./zmk-cornedeon54/build.sh dongle
+
+#workspaces/zmk# ./zmk-cornedeon54/build.sh left
+
+#workspaces/zmk# ./zmk-cornedeon54/build.sh right
---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+
+# Keyboard name
+KEYBOARD="cornedeon54"
+
+# Paths
+CONFIG_DIR="/workspaces/zmk/zmk-${KEYBOARD}/config"
+MODULES_DIR="/workspaces/zmk/zmk-${KEYBOARD}"
+OUTPUT_DIR="/workspaces/zmk/zmk-${KEYBOARD}/firmware"
+
+if [ -z "$1" ]; then
+    echo "X: shield name not specified."
+    echo "Usage: $0 <shield_name>"
+    exit 1
+fi
+
+SHIELD="${KEYBOARD}_$1"
+
+echo "=== Build: ${SHIELD} ==="
+
+west build -s app -p -b nice_nano -S studio-rpc-usb-uart -- \
+  -DSHIELD="$SHIELD" \
+  -DZMK_CONFIG="$CONFIG_DIR" \
+  -DZMK_EXTRA_MODULES="$MODULES_DIR" \
+  -DCONFIG_ZMK_STUDIO=y
+
+if [ $? -ne 0 ]; then
+    echo "X: build error"
+    exit 1
+fi
+
+mkdir -p "$OUTPUT_DIR"
+SOURCE_FILE="build/zephyr/zmk.uf2"
+TARGET_FILE="$OUTPUT_DIR/zmk_${SHIELD}.uf2"
+
+if [ -f "$SOURCE_FILE" ]; then
+    cp "$SOURCE_FILE" "$TARGET_FILE"
+    echo "=== Success! uf2 copied to: $TARGET_FILE ==="
+else
+    echo "x: ${SOURCE_FILE} not found"
+    exit 1
+fi
